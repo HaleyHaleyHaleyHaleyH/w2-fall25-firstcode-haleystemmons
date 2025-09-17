eca28b029ec020c5246a32e7e729ae0dbaa954b4
HaleyHaleyHaleyHaleyH <haleystemmons@alum.calarts.edu>
2025-09-16 18:22:08 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  // Create a canvas that fills the entire browser window
+  createCanvas(500, 500);
+}
+// the person reading this code is gay
+//har har har har
+function draw() {
+background(200,120,200);
+
+circle(250,250,450);
+}
